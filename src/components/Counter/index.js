5cb17cf3897eec106925c1332f3c314b02571f2c
prashanth <prash@gmail>
2023-03-18 18:14:06 +0000

    using localStorage

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,23 +1,50 @@
 import {Component} from 'react'
 import './index.css'
+let countValue
+let color
+let countV = JSON.parse(localStorage.getItem('count'))
+if (countV === null) {
+  countValue = 0
+} else {
+  countValue = countV
+}
 
 class Counter extends Component {
-  state = {count: 0}
+  state = {count: countValue}
   onIncrement = () => {
     this.setState(prevState => {
-      //   console.log(`previous State value: ${prevState.count}`)
-      return {
-        count: prevState.count + 1,
+      const countingValue = prevState.count + 1
+      if (countingValue > 0) {
+        color = 'green'
+      } else if (countingValue < 0) {
+        color = 'red'
+      } else {
+        color = 'black'
       }
+
+      localStorage.setItem('count', JSON.stringify(countingValue))
+      return {count: countingValue}
     })
   }
 
   onDecrement = () => {
     this.setState(prevState => {
-      return {count: prevState.count - 1}
+      const countingValue = prevState.count - 1
+      localStorage.setItem('count', JSON.stringify(countingValue))
+      if (countingValue > 0) {
+        color = 'green'
+      } else if (countingValue < 0) {
+        color = 'red'
+      } else {
+        color = 'black'
+      }
+
+      return {count: countingValue}
     })
   }
   onReset = () => {
+    localStorage.setItem('count', '0')
+    color = 'black'
     this.setState({count: 0})
   }
 
@@ -26,7 +53,7 @@ class Counter extends Component {
     return (
       <div className="container">
         <h1 className="heading">Counter</h1>
-        <p className="count">{count}</p>
+        <p className={`count ${color}`}>{count}</p>
         <div>
           <button className="button" onClick={this.onIncrement}>
             Increase
